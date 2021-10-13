d940e582e0b4a20d485a4bca71842c4d31c46070
gayatrivjoshi <gayatrij768@gmail.com>
2021-10-14 01:22:21 +0530

    Style and add card component

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./Board.css";
 import Card from "../Card/Card";
+import { MoreHorizontal } from "react-feather";
+// import DotLogo from "../../Icons/3dots.svg"
 
 function Board() {
   return (
     <div className="board">
       <div className="board_top">
         <p className="board_top_title">To Do</p>
+        <MoreHorizontal />
       </div>
       <div className="board_cards">
         <Card />
--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,18 +1,20 @@
 import React from 'react'
+import {MoreHorizontal} from "react-feather";
+import {Clock} from "react-feather";
+import "./Card.css"
 
 function Card() {
     return (
-        <div className="card">
-            <div className="card_top">
-                <div className="card_title">
-                    hdjshfdnv vjfkvn
-                </div>
-                <div className="card_footer">
-                    
-                </div>
-            </div>
+      <div className="cards">
+        <div className="card_top">
+            <span className="card_title">sjfhjsdch</span>
+            <MoreHorizontal />
         </div>
-    )
+        <div className="card_footer">
+            <p><Clock />12 Oct</p>
+        </div>
+        </div>
+    );
 }
 
 export default Card
